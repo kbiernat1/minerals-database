c48b0a9f93e1ce77f1f114050c94e0c6abc74939
Kamil Biernat <k.biernat@yahoo.ca>
2022-09-26 12:55:28 +0200

    detail changes

--- src/main/java/com/crud/minerals/controller/MineralSearchController.java
+++ src/main/java/com/crud/minerals/controller/MineralSearchController.java
@@ -26,7 +26,7 @@ public class MineralSearchController {
         return "mineralSearch";
     }
 
-   /* @PostMapping()
+   @PostMapping()
     public String getMineralsByDifferentParameters(@RequestParam(name = "name", required = false) String name, @RequestParam(name = "color", required = false) String color,
                                                    @RequestParam(name = "shine", required = false) String shine, @RequestParam(name = "fragility", required = false) String fragility,
                                                    @RequestParam(name = "transparency", required = false) String transparency, @RequestParam(name = "opalescence", required = false)
@@ -35,14 +35,5 @@ public class MineralSearchController {
         List<Mineral> minerals = dbService.retrieveByDifferentParameters(name, color, shine, fragility, transparency, opalescence, region);
         model.addAttribute("minerals", minerals);
         return "mineralSearch";
-    }*/
-
-    @GetMapping("/mineralSearch")
-    public ResponseEntity<List<MineralDto>> getMinerals (@RequestParam(name = "name", required = false) String name, @RequestParam(name = "color", required = false) String color,
-                                                         @RequestParam(name = "shine", required = false) String shine, @RequestParam(name = "fragility", required = false) String fragility,
-                                                         @RequestParam(name = "transparency", required = false) String transparency, @RequestParam(name = "opalescence", required = false)
-                                                                     Character opalescence, @RequestParam(name = "region", required = false) String region, Model model) {
-        List<Mineral> minerals = dbService.retrieveByDifferentParameters(name, color, shine, fragility, transparency, opalescence, region);
-        return ResponseEntity.ok(mineralMapper.mineralToDtoList(minerals));
     }
 }
